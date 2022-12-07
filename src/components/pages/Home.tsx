e18d4aea134c79a6d92bd797bee3dc1372b9ee69
reactif974 <p.perroudon69@gmail.com>
2022-12-07 01:09:59 +0100

    :heavy_plus_sign: F01 > Add types/node dependencies and add bacground in Home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
-    return(
-        <div>Home</div>
-    )
+import styled from "styled-components";
+import homeBg from "../../assets/reactif_dev.jpg";
+
+interface Props {
+  bg: string;
 }
+
+export default function Home() {
+  return <HomeStyled bg={homeBg}>Home</HomeStyled>;
+}
+
+const HomeStyled = styled.div<Props>`
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(15, 17, 19, 1),
+      rgba(62, 63, 112, 0.7),
+      rgba(15, 17, 19, 1)
+    ),
+    url(${(props) => props.bg}) no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
