7dcf4eee09893899eebd42bde9f8bd4ee6ca71f7
reactif974 <p.perroudon69@gmail.com>
2022-12-15 18:17:18 +0100

    :lipstick: FO1 > Home > Header > add menu icon

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,6 +1,7 @@
-import styled from "styled-components";
-import homeBg from "../../assets/reactif_dev.jpg";
-import logo from "../../assets/logo-creactif.png";
+import styled from "styled-components"
+import homeBg from "../../assets/reactif_dev.jpg"
+import logo from "../../assets/logo-creactif.png"
+import {FaHamburger} from "react-icons/fa"
 
 interface Props {
   bg: string;
@@ -14,6 +15,9 @@ export default function Home() {
         <h1 className="logo">
           <img src={logo} alt="logo reactif" width="250px"/>
         </h1>
+        <div className="navigation-icon">
+          <FaHamburger />
+        </div>
       </header>
     </HomeStyled>
   );
@@ -26,9 +30,21 @@ const HomeStyled = styled.div<Props>`
     display: flex;
     height:120px;
     justify-content: space-between;
+    align-items: center;
     background-color:blue;
+    padding-right: 20px;
     .logo {
       padding-left:20px;
+    }
+    .navigation-icon {
+      font-size:30px;
+      &::before {
+        content:"MENU";
+        position:absolute;
+        font-size: 0.6em;
+        top:48px;
+        right:60px;
+      }
     }
   }
 `;
